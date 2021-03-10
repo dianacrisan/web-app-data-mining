bb265e41845b1275084d146be534bf512775f3f2
Diana Crisan <crisanmonica18@stud.ase.ro>
2021-03-10 11:00:36 +0200

    scraper preparations complete

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,6 +7,11 @@ router.get('/', (req, res) => {
     res.send("Welcome to your web scraper page!");
 });
 
-router.get('/scraper', scraperController.getDataFromUrl);
+// Sraper using JavaScript
+// router.get('/scraper', scraperController.getDataFromUrl);
+
+router.get('/scraper', (req, res) => {
+    res.send("Scraper implementation in progress...")
+});
 
 module.exports = router;
