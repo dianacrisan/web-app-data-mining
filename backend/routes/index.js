12f782ecc417dd33fbb80e0f0c847f4f8fa321f1
Diana Crisan <crisanmonica18@stud.ase.ro>
2021-04-27 13:48:05 +0300

    user interface update

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const router = express();
 
-const scraperController = require('../controllers/Scraper');
-
 router.get('/', (req, res) => {
-    res.send("Welcome to your web scraper page!");
+     res.send("Welcome to your web scraper page!");
 });
 
-router.get('/scraper', (req, res) => {
-    res.send("Scraper implementation in progress...");
-});
+// router.get('/scraper', (req, res) => {
+//     res.send("Scraper implementation in progress...");
+// });
 
 module.exports = router;
